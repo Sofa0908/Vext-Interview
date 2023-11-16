acdcf5b42ca5cb726c060c30b1cfc4e67ad2f7e3
Vincent Yang <71572921+Sofa0908@users.noreply.github.com>
2023-11-16 21:11:56 +0800

    venv stuff

--- gen_test.py
+++ gen_test.py
@@ -1,16 +1,40 @@
 from langchain.chains import LLMChain
 from langchain.llms import Cohere
-from langchain.prompts import PromptTemplate
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
 
-template = """Question: {question}
-Answer: Let's think step by step."""
-
-prompt = PromptTemplate(template=template, input_variables=["question"])
+prompt = ChatPromptTemplate(messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a car sales that is trying to make a sale."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ])
 
 llm = Cohere(cohere_api_key="DTwV9xX1ce0k2jXy3HrC7TfoxVRMFxZr3kURv5TV")
-llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
 
-print(question)
-print(llm_chain.run(question))
+# Notice that we just pass in the `question` variables - `chat_history` gets populated by memory
+conversation({"question": "hi"})
+
+while True:
+    message = input("User: ")
+    
+    # Typing "quit" ends the conversation
+    if message.lower() == "quit":
+        print("Ending chat.")
+        break
+
+    # Chatbot response
+    response = conversation({"question": message})
+    print(response['text'])
--- vext_interview/chat/views.py
+++ vext_interview/chat/views.py
@@ -1,10 +1,38 @@
+import json
+
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from rest_framework.decorators import api_view
 
+from langchain.chains import LLMChain
+from langchain.llms import Cohere
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
+
+prompt = ChatPromptTemplate(messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a car sales that is trying to make a sale."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ])
+
+llm = Cohere(cohere_api_key="DTwV9xX1ce0k2jXy3HrC7TfoxVRMFxZr3kURv5TV")
+
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+
+@api_view(['POST'])
 def chat(request):
-    if request.method == 'POST':
-        return JsonResponse({'bot_response': 'Hi POST'})
-    else:
-        template = loader.get_template('bot.html')
-    
-    return HttpResponse(template.render())
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    response = conversation({"question": body['question']})
+    return JsonResponse({'bot_response': response['text']})
