3b41e749994cc6b2168cab1081d0d035938e2d5b
Vincent Yang <71572921+Sofa0908@users.noreply.github.com>
2023-11-17 16:05:11 +0800

    Add architecture diagram

--- vext_interview/chat/views.py
+++ vext_interview/chat/views.py
@@ -1,7 +1,6 @@
 import json
 
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from langchain.chains import LLMChain
@@ -18,7 +17,7 @@ llm = Cohere(cohere_api_key="DTwV9xX1ce0k2jXy3HrC7TfoxVRMFxZr3kURv5TV")
 
 prompt = ChatPromptTemplate(messages=[
         SystemMessagePromptTemplate.from_template(
-            "You are a car sales that is trying to make a sale."
+            "You are geologist, volcano specialist."
         ),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{question}"),
@@ -32,4 +31,4 @@ def chat(request):
     body_unicode = request.body.decode("utf-8")
     body = json.loads(body_unicode)
     response = conversation({"question": body["question"]})
-    return JsonResponse({"bot_response": response["text"]})
+    return JsonResponse({"bot_response": response["text"]}) 
